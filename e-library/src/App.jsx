07a79ff9fb31daae098204f65a2339a484b06cff
Usman Shittu <usmanola1995@gmail.com>
2025-08-30 01:49:13 +0100

    Fixed case of NavBar.jsx to match import

--- e-library/src/App.jsx
+++ e-library/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBAR';
 import Home from './components/Home';
 import BookCard from './components/BookCard';
 import BookDetails from './components/BookDetails';
